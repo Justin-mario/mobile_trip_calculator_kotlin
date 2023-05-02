34f8a3bba8be76513b7d1faa5f1f92b4e613e58e
Justin-mario <79143260+Justin-mario@users.noreply.github.com>
2023-05-02 23:20:38 +0100

    third commit

--- app/src/main/java/com/example/android/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/android/tiptime/MainActivity.kt
@@ -1,3 +1,5 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.example.android.tiptime
 
 import android.os.Bundle
@@ -5,16 +7,23 @@ import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.StringRes
+import androidx.annotation.VisibleForTesting
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Switch
+import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -25,8 +34,12 @@ import androidx.compose.runtime.setValue
 
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -51,12 +64,14 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
+
 @Composable
 fun EditNumberField(@StringRes label: Int,
                     value: String,
                     onValueChange: (String) -> Unit,
-                    modifier: Modifier = Modifier){
+                    keyboardOptions: KeyboardOptions,
+                    keyboardActions: KeyboardActions,
+                    modifier: Modifier = Modifier) {
 
     TextField(
         value = value,
@@ -66,17 +81,35 @@ fun EditNumberField(@StringRes label: Int,
         label = { Text(text = stringResource(label))
         },
         singleLine = true,
-        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+        keyboardOptions = keyboardOptions,
+        keyboardActions = keyboardActions
     )
 
 
 
 }
-
 @Composable
-private fun calculateTip(amount: Double, tipPercent: Double ): String{
-    val tip = tipPercent/100 * amount
+fun RoundTheTipRow(roundUp: Boolean, onRoundUpChanged: (Boolean) -> Unit,
+                   modifier: Modifier = Modifier) {
+    Row(modifier = modifier
+        .fillMaxWidth()
+        .size(48.dp),
+    verticalAlignment = Alignment.CenterVertically) {
+        Text(text = stringResource(id = R.string.round_up_tip))
+        Switch(checked = roundUp, onCheckedChange = onRoundUpChanged,
+        modifier = modifier
+            .fillMaxWidth()
+            .wrapContentWidth(Alignment.End),
+        colors = SwitchDefaults.colors(uncheckedThumbColor = Color.Gray))
+    }
 
+}
+@VisibleForTesting
+internal fun calculateTip(amount: Double, tipPercent: Double, roundUp: Boolean ): String {
+    var tip = tipPercent/100 * amount
+    if (roundUp){
+        tip = kotlin.math.ceil(tip)
+    }
     return NumberFormat.getCurrencyInstance().format(tip)
 }
 
@@ -84,11 +117,12 @@ private fun calculateTip(amount: Double, tipPercent: Double ): String{
 fun TipTimeScreen(modifier: Modifier = Modifier) {
     var amountInput by  remember{ mutableStateOf("") }
     var tipInput  by  remember{ mutableStateOf("") }
+    var roundUp by remember { mutableStateOf(false) }
     
     val amount = amountInput.toDoubleOrNull() ?: 0.0
     val tipPercent = tipInput.toDoubleOrNull() ?: 0.0
-
-    val tip = calculateTip(amount = amount, tipPercent)
+    val tip = calculateTip(amount = amount, tipPercent, roundUp = roundUp)
+    val focus = LocalFocusManager.current
 
 
     Column(modifier = Modifier.padding(32.dp),
@@ -101,14 +135,24 @@ fun TipTimeScreen(modifier: Modifier = Modifier) {
                 .align(Alignment.CenterHorizontally)
         )
         Spacer(modifier = Modifier.height(16.dp))
-        EditNumberField(R.string.bill_amount,amountInput, {amountInput = it})
+        EditNumberField(R.string.bill_amount,amountInput, {amountInput = it},
+            keyboardOptions =  KeyboardOptions.Default.copy(
+            keyboardType = KeyboardType.Number,
+            imeAction = ImeAction.Next),
+            keyboardActions = KeyboardActions(onDone = {focus.clearFocus()}))
         Spacer(modifier = modifier.height(12.dp))
-        EditNumberField(R.string.how_was_the_service,tipInput, {tipInput = it})
+        EditNumberField(R.string.how_was_the_service,tipInput, {tipInput = it},
+            keyboardOptions =  KeyboardOptions.Default.copy(
+            keyboardType = KeyboardType.Number,
+            imeAction = ImeAction.Done), keyboardActions = KeyboardActions(onNext = {focus.moveFocus(
+                FocusDirection.Down)}))
+        RoundTheTipRow(roundUp = roundUp, onRoundUpChanged = {roundUp = it} )
         Spacer(modifier = Modifier.height(24.dp))
         Text(text = stringResource(id = R.string.tip_amount, tip),
         fontWeight = FontWeight.Bold,
         fontSize = 20.sp,
         )
+
     }
 
 }
---
+++ app/src/test/java/com/example/tiptime/TipCalculatorTests.kt
@@ -0,0 +1,20 @@
+package com.example.tiptime
+
+import com.example.android.tiptime.calculateTip
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
+import java.text.NumberFormat
+
+class TipCalculatorTests {
+
+    @Test
+    fun calculate_20_percent_tip_no_roundup(){
+        val amount = 10.00
+        val tipPercent = 20.00
+        val expectedTip = NumberFormat.getCurrencyInstance().format(2)
+        val actualTip = calculateTip(amount = amount, tipPercent = tipPercent, roundUp = false)
+
+        assertEquals(expectedTip, actualTip)
+
+    }
+}
