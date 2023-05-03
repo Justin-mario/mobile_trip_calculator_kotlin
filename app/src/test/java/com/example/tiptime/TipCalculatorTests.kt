c6716598887501a1f8eea6c95cd741ac88929585
Justin-mario <79143260+Justin-mario@users.noreply.github.com>
2023-05-03 01:34:43 +0100

    added local test and implementation test

--- app/src/test/java/com/example/tiptime/TipCalculatorTests.kt
+++ app/src/test/java/com/example/tiptime/TipCalculatorTests.kt
@@ -8,7 +8,7 @@ import java.text.NumberFormat
 class TipCalculatorTests {
 
     @Test
-    fun calculate_20_percent_tip_no_roundup(){
+    fun calculate_20_percent_tip_no_roundup() {
         val amount = 10.00
         val tipPercent = 20.00
         val expectedTip = NumberFormat.getCurrencyInstance().format(2)
